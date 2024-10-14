7ce7e528d74392fb21427512ae9b671b0606c67f
Ashraf12356 <ashraffouhaddou@outlook.com>
2024-10-14 11:50:21 +0200

    Add files via upload

---
+++ 1.1lol.js
@@ -0,0 +1,8 @@
+const date1 = new Date(1985, 6, 12);
+const date2 = new Date(1955, 8, 23);
+const date3 = new Date(2015, 7, 31); 
+const date4 = new Date(1885, 0, 4);
+console.log("12 Juli 1985: " + date1);
+console.log("23 September 1955: " + date2);
+console.log("31 Augustus 2015: " + date3);
+console.log("4 Januari 1885: " + date4);
