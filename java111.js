754993884078f9231e3fcca8f7c1fda1a59130aa
Ashraf12356 <ashraffouhaddou@outlook.com>
2024-10-14 11:46:28 +0200

    Add files via upload

---
+++ java111.js
@@ -0,0 +1,23 @@
+let name = "vegeta";
+let jumpforce = 2000; // 
+let specialPower = "teleport"; 
+let speed = 280; 
+let bike = "Honda VFR VTEC";
+let livesLeft = 11; 
+let ammo = 121; 
+let numberOfLasers = 52; 
+let batteryPower = 90;
+let level =29000; 
+let wanted = true; 
+
+console.log("name: " + name);
+console.log("jumpforce: " + jumpforce);
+console.log("special power: " + specialPower);
+console.log("speed: " + speed);
+console.log("bike: " + bike);
+console.log("lives left: " + livesLeft);
+console.log("ammo: " + ammo);
+console.log("number of lasers: " + numberOfLasers);
+console.log("battery power: " + batteryPower);
+console.log("level: " + level);
+console.log("wanted: " + wanted);
